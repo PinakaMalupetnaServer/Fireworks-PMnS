9a4a18ebf99bab9c42aaacb4a5337a51e1e04779
Frizth Lyco Tatierra <lycol50@gmail.com>
2021-12-10 01:57:56 +0800

    Add files via upload

--- src/princepines/FireworksPMnS/main.php
+++ src/princepines/FireworksPMnS/main.php
@@ -29,17 +29,6 @@ class main extends PluginBase implements Listener
             case "launch":
                 if ($sender instanceof Player) {
                     $sender->sendMessage("Fireworks Starting.");
-                    $player = $this->getServer()->getOnlinePlayers();
-                    if ($player instanceof Player){
-                        $pk = new PlaySoundPacket;
-                        $pk->soundName = "medley";
-                        $pk->x = (int)$player->x;
-                        $pk->y = (int)$player->y;
-                        $pk->z = (int)$player->z;
-                        $pk->volume = 1;
-                        $pk->pitch = 1;
-                        $player->dataPacket($pk);
-                    }
                     $task = new FireworkTask();
                     $this->tasks[$sender->getId()] = $task;
                     $this->getScheduler()->scheduleDelayedRepeatingTask($task, 20,20);
