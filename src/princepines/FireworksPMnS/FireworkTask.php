3a68c17e6dff3590e2d65c639b22fae16a3f57c2
Frizth Lyco Tatierra <lycol50@gmail.com>
2021-12-09 19:19:23 +0800

    update
    more changes

--- src/princepines/FireworksPMnS/FireworkTask.php
+++ src/princepines/FireworksPMnS/FireworkTask.php
@@ -24,31 +24,31 @@ class FireworkTask extends Task
         $max = 3;
         /** @var Fireworks $fw */
         $fw = ItemFactory::get(Item::FIREWORKS);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_BLACK, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_SMALL_SPHERE, Fireworks::COLOR_BLACK, "", false, true);
         $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_RED, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_GREEN, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_BURST, Fireworks::COLOR_DARK_GREEN, "", false, true);
         $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_BROWN, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_BLUE, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_PURPLE, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_AQUA, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_SMALL_SPHERE, Fireworks::COLOR_BLUE, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_BURST, Fireworks::COLOR_DARK_PURPLE, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_SMALL_SPHERE, Fireworks::COLOR_DARK_AQUA, "", false, true);
         $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_GRAY, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_GRAY, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_BURST, Fireworks::COLOR_DARK_GRAY, "", false, true);
         $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_PINK, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_GREEN, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_YELLOW, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_LIGHT_AQUA, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_SMALL_SPHERE, Fireworks::COLOR_GREEN, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_BURST, Fireworks::COLOR_YELLOW, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_SMALL_SPHERE, Fireworks::COLOR_LIGHT_AQUA, "", false, true);
         $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_PINK, "", false, true);
-        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_GOLD, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_BURST, Fireworks::COLOR_GOLD, "", false, true);
         $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_WHITE, "", false, true);
         $fw->setFlightDuration(mt_rand($min, $max));
 
         // LOBBY
         // Use whatever level you'd like here. Must be loaded
         $level = Server::getInstance()->getLevelByName("lobby");
+
         $posArray = [new Vector3(262, 72, 341), new Vector3(262, 72, 334),
             new Vector3(262, 72, 321), new Vector3(262, 72, 310),
-            new Vector3(325,76,334), new Vector3(326,76,316),
-            new Vector3(274,66,349), new Vector3(274,66,301)];
+            new Vector3(325,76,334), new Vector3(326,76,316)];
         foreach ($posArray as $array) {
             $getBlockPos = $level->getBlock($array);
             // Choose some coordinates
@@ -60,13 +60,13 @@ class FireworkTask extends Task
             if ($entity instanceof FireworksRocket) {
                 $entity->spawnToAll();
             }
+            $level->setTime(18000);
         }
 
         // EVENTS
         // Use whatever level you'd like here. Must be loaded
         $level2 = Server::getInstance()->getLevelByName("Event");
-        $posArray2 = [new Vector3(386,25,247), new Vector3(387,25,229), 
-            new Vector3(418,4,163), new Vector3(446,4,163),
+        $posArray2 = [new Vector3(386,25,247), new Vector3(387,25,229),
             new Vector3(433,25,308), new Vector3(418,25,308),
             new Vector3(475,34,211), new Vector3(475,34,265)];
         foreach ($posArray2 as $array2) {
@@ -80,6 +80,7 @@ class FireworkTask extends Task
             if ($entity2 instanceof FireworksRocket) {
                 $entity2->spawnToAll();
             }
+            $level->setTime(18000);
         }
     }
 }
