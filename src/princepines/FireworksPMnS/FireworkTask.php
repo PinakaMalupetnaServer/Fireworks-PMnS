cc70ef9d1daad04dd4faacab0b7848975fdb00a6
Frizth Lyco Tatierra <lycol50@gmail.com>
2021-12-09 10:38:08 +0800

    update malala
    used tasks instead of whole one file code.

---
+++ src/princepines/FireworksPMnS/FireworkTask.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace princepines\FireworksPMnS;
+
+
+use BlockHorizons\Fireworks\entity\FireworksRocket;
+use BlockHorizons\Fireworks\item\Fireworks;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\math\Vector3;
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+
+class FireworkTask extends Task
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function onRun(int $currentTick)
+    {
+        $min = 1;
+        $max = 3;
+        /** @var Fireworks $fw */
+        $fw = ItemFactory::get(Item::FIREWORKS);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_BLACK, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_RED, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_GREEN, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_BROWN, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_BLUE, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_PURPLE, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_AQUA, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_GRAY, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_GRAY, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_PINK, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_GREEN, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_YELLOW, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_LIGHT_AQUA, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_DARK_PINK, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_GOLD, "", false, true);
+        $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, Fireworks::COLOR_WHITE, "", false, true);
+        $fw->setFlightDuration(mt_rand($min, $max));
+
+        // Use whatever level you'd like here. Must be loaded
+        $level = Server::getInstance()->getLevelByName("lobby");
+        $posArray = [new Vector3(262, 72, 341), new Vector3(262, 72, 334), new Vector3(262, 72, 321), new Vector3(262, 72, 310)];
+        foreach ($posArray as $array) {
+            $getBlockPos = $level->getBlock($array);
+            // Choose some coordinates
+            $pos = $getBlockPos->add(0.5, 1, 0.5);
+            //$vector3 = $level->getSpawnLocation()
+            // Create the NBT data
+            $nbt = FireworksRocket::createBaseNBT($pos, new Vector3(0.001, 0.05, 0.001), lcg_value() * 360, 90);
+            // Construct and spawn
+            $entity = FireworksRocket::createEntity("FireworksRocket", $level, $nbt, $fw);
+            if ($entity instanceof FireworksRocket) {
+                $entity->spawnToAll();
+            }
+        }
+    }
+}
--- src/princepines/FireworksPMnS/main.php
+++ src/princepines/FireworksPMnS/main.php
@@ -2,21 +2,13 @@
 
 namespace princepines\FireworksPMnS;
 
-use pocketmine\block\Block;
-use pocketmine\block\StoneBricks;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
-use pocketmine\item\Item;
-use pocketmine\item\ItemFactory;
-use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\Task;
-use pocketmine\Server;
 
-use BlockHorizons\Fireworks\item\Fireworks;
-use BlockHorizons\Fireworks\entity\FireworksRocket;
 
 class main extends PluginBase implements Listener
 {
@@ -27,42 +19,23 @@ class main extends PluginBase implements Listener
         $this->getLogger()->info("Fireworks Enabled");
     }
 
+    public $tasks = [];
+
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
         switch ($command->getName()) {
             case "launch":
                 if ($sender instanceof Player) {
-                    $this->getScheduler()->scheduleDelayedRepeatingTask(new class extends \pocketmine\scheduler\Task{
-                        public function onRun(int $currentTick) : void{
-                            $fwColors = [Fireworks::COLOR_BLACK, Fireworks::COLOR_RED, Fireworks::COLOR_DARK_GREEN, Fireworks::COLOR_BROWN, Fireworks::COLOR_BLUE, Fireworks::COLOR_DARK_PURPLE, Fireworks::COLOR_DARK_AQUA, Fireworks::COLOR_GRAY, Fireworks::COLOR_DARK_GRAY, Fireworks::COLOR_PINK, Fireworks::COLOR_GREEN, Fireworks::COLOR_YELLOW, Fireworks::COLOR_LIGHT_AQUA, Fireworks::COLOR_DARK_PINK, Fireworks::COLOR_GOLD, Fireworks::COLOR_WHITE];
-                            $min = 1;
-                            $max = 3;
-                            /** @var Fireworks $fw */
-                            $fw = ItemFactory::get(Item::FIREWORKS);
-                            $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, array_rand($fwColors), "", false, false);
-                            $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, array_rand($fwColors), "", false, false);
-                            $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, array_rand($fwColors), "", false, false);
-                            $fw->addExplosion(Fireworks::TYPE_HUGE_SPHERE, array_rand($fwColors), "", false, false);
-                            $fw->setFlightDuration(mt_rand($min, $max));
-
-                            // Use whatever level you'd like here. Must be loaded
-                            $level = Server::getInstance()->getLevelByName("lobby");
-                            $posArray = [new Vector3(262, 72, 341), new Vector3(262, 72, 334), new Vector3(262, 72, 321), new Vector3(262, 72, 310)];
-                            foreach ($posArray as $array) {
-                                $getBlockPos = $level->getBlock($array);
-                                // Choose some coordinates
-                                $pos = $getBlockPos->add(0.5, 1, 0.5);
-                                //$vector3 = $level->getSpawnLocation()
-                                // Create the NBT data
-                                $nbt = FireworksRocket::createBaseNBT($pos, new Vector3(0.001, 0.05, 0.001), lcg_value() * 360, 90);
-                                // Construct and spawn
-                                $entity = FireworksRocket::createEntity("FireworksRocket", $level, $nbt, $fw);
-                                if ($entity instanceof FireworksRocket) {
-                                    $entity->spawnToAll();
-                                }
-                            }
-                        }
-                    }, 20,20);
+                    $task = new FireworkTask();
+                    $this->tasks[$sender->getId()] = $task;
+                    $this->getScheduler()->scheduleDelayedRepeatingTask($task, 20,20);
+                }
+                break;
+            case "stoplaunch":
+                if ($sender instanceof Player) {
+                    $task = $this->tasks[$sender->getId()];
+                    unset($this->tasks[$sender->getId()]);
+                    $task->getHandler()->cancel();
                 }
                 break;
         }
